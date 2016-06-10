7d39ba94eb000922b89bbc01e11ffb16aa0f99c2
Tobias Neumann <t.neumann@fh-aachen.de>
2016-06-10 15:02:43 +0200

    comm_example: add minimal client server example using protobuf_comm

---
+++ comm_example/server.cpp
@@ -0,0 +1,137 @@
+
+/***************************************************************************
+ * client.cpp - 
+ *
+ *  Created: Mon Mar 04 14:09:00 2013
+ *  Copyright  2013-2015  Tim Niemueller [www.niemueller.de]
+ *             2016 Tobias Neumann
+ ****************************************************************************/
+
+/*  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of the authors nor the names of its contributors
+ *   may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <protobuf_comm/peer.h>
+
+#include <boost/asio.hpp>
+#include <boost/date_time.hpp>
+#include "cs_comm.pb.h"
+
+using namespace protobuf_comm;
+
+static bool quit = false;
+ProtobufBroadcastPeer *peer_public_ = NULL;
+
+void
+signal_handler(const boost::system::error_code& error, int signum)
+{
+  if (!error) {
+    quit = true;
+  }
+}
+
+void
+handle_recv_error(boost::asio::ip::udp::endpoint &endpoint, std::string msg)
+{
+  printf("Receive error from %s:%u: %s\n",
+     endpoint.address().to_string().c_str(), endpoint.port(), msg.c_str());
+}
+
+void
+handle_send_error(std::string msg)
+{
+  printf("Send error: %s\n", msg.c_str());
+}
+
+void
+handle_message(boost::asio::ip::udp::endpoint &sender,
+	       uint16_t component_id, uint16_t msg_type,
+	       std::shared_ptr<google::protobuf::Message> msg)
+{
+  std::shared_ptr<upns::Request> rq;
+  if ((rq = std::dynamic_pointer_cast<upns::Request>(msg))) {
+
+    if ( rq->type() == upns::Request::TEXT_OUTPUT ) {
+      printf("Got request \"TEXT_OUTPUT\" from \"%s\", will send no reply\n", rq->sender_name().c_str());
+    } else if ( rq->type() == upns::Request::POINTCLOUD ) {
+      printf("Got request \"POINTCLOUD\" from \"%s\", will send pointcloud\n", rq->sender_name().c_str());
+
+      upns::PointCloud pc;
+      pc.set_pointcloud("abc");
+
+      peer_public_->send(pc);
+    }
+
+  } else {
+
+    printf("RECEIVED UNKNOWN Protobuf msg\n");
+
+  }
+}
+
+int
+main(int argc, char **argv)
+{
+  peer_public_ = new ProtobufBroadcastPeer(
+        "127.0.0.1",
+        4445,
+        4444
+      );
+//    peer_public_ = new ProtobufBroadcastPeer(config_->get_string("/llsfrb/comm/public-peer/host"),
+//					     config_->get_uint("/llsfrb/comm/public-peer/port"));
+
+  MessageRegister & message_register = peer_public_->message_register();
+  message_register.add_message_type<upns::Request>();
+//  message_register.add_message_type<GameState>();
+//  message_register.add_message_type<MachineInfo>();
+
+  boost::asio::io_service io_service;
+
+  printf("ready to receive requests from upns partners...\n");
+
+  peer_public_->signal_received().connect(handle_message);
+  peer_public_->signal_recv_error().connect(handle_recv_error);
+  peer_public_->signal_send_error().connect(handle_send_error);
+
+#if BOOST_ASIO_VERSION >= 100601
+  // Construct a signal set registered for process termination.
+  boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
+
+  // Start an asynchronous wait for one of the signals to occur.
+  signals.async_wait(signal_handler);
+#endif
+
+  do {
+    io_service.run();
+    io_service.reset();
+  } while (! quit);
+
+  delete peer_public_;
+
+  // Delete all global objects allocated by libprotobuf
+  google::protobuf::ShutdownProtobufLibrary();
+}
